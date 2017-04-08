66662bd0bec87937cd1bfb813d2fe3f9fd4bfbac
abc45628 <abc45628@163.com>
2017-04-09 00:04:47 +0800

    fix target="_blank"

--- steamcommunity/steamInventory.js
+++ steamcommunity/steamInventory.js
@@ -1,29 +1,48 @@
 // ==UserScript==
-// @name        steam低价卖卡
+
+// @name        steam仓库
+
 // @namespace   https://git.oschina.net/abc45628/steam-low-price
+
 // @version     0.1
+
 // @author      abc45628
-// @description 取国区最低出价，为求最快卖出
+
+// @description steam仓库
+
 // @supportURL  https://git.oschina.net/abc45628/steam-low-price/issues
+
 // @include     /^https?:\/\/steamcommunity\.com\/.*\/inventory/
+
 // @license     MIT
+
 // @run-at      document-end
+
 // ==/UserScript==
+
 
 var $J=unsafeWindow.$J;
 var user64=document.URL.match('[0-9]{4,}')[0];//steam 64位id
+
 var cards=[
   //card的模板
+
   //var	assetid;//每张卡的标识,多个assetid可能对应同一个classid,多个classid对应同一个app
+
   //var classid;//每张卡的名字
+
   //var market_hash_name;//查市场价格用到
+
   //var appid;//游戏的app
+
 ];
 var items={"cards":cards,"last_assetid":undefined,"total_inventory_count":undefined};
 var getInvData_progress=false;
 var loadCount=0;//重试加载仓库数据的次数
 
+
 /**得到仓库数据
+
 *  start_assetid   每张卡都有一个assetid，就算是2张相同的卡，assetid都会不同*/
 function getInventoryData(start_assetid){
   if(getInvData_progress){ return; }
@@ -31,6 +50,7 @@ function getInventoryData(start_assetid){
   var params = {
     l: 'schinese',
     count: 25//请求的物品数量
+
   };
   if(start_assetid){
     params.start_assetid=start_assetid;
@@ -48,6 +68,7 @@ function getInventoryData(start_assetid){
         for(;asset.classid !== descriptions[desc_used].classid;){desc_used++;}
         card.market_hash_name=descriptions[desc_used].market_hash_name;
         //宝珠没有appid
+
         var appid=card.market_hash_name.match('[0-9]{6,}');
         if(appid){card.appid=appid[0];}
         cards.push(card);
@@ -55,6 +76,7 @@ function getInventoryData(start_assetid){
       items.last_assetid=data.last_assetid;
       items.total_inventory_count=data.total_inventory_count;
       // console.log("length="+cards.length);
+
       getInvData_progress=false;
       loadCount=0;
       console.log("仓库数据");
@@ -71,6 +93,7 @@ function getInventoryData(start_assetid){
 
 function addUI(){
   // 插入出售按钮
+
   var sell_btn=document.createElement('div');
   sell_btn.innerHTML='<a id="sce_page" target="_blank" class="item_market_action_button item_market_action_button_green">'
     +'<span class="item_market_action_button_edge item_market_action_button_left"></span>'
@@ -81,12 +104,14 @@ function addUI(){
   document.addEventListener('click',function (ev){
     var evt=ev.target;
     // 点击卡片
+
     if (evt.className === 'inventory_item_link') {
 
       console.log("hash"+evt.hash);
       clickCard(evt.hash);
     }
     //点击下一页
+
     else if(evt.id === 'pagebtn_next'){
       var cur_page=document.querySelector('#pagecontrol_cur').textContent;
       if(cards.length<items.total_inventory_count && cards.length-cur_page*25<=50){
@@ -99,18 +124,27 @@ function addUI(){
 }
 
 // 点击卡片
+
 function clickCard(url){
 
   document.querySelector("#sce_page .item_market_action_button_contents").textContent="steamcardexchange页面";
   var asset=url.split('_');
   //卡片、宝珠、表情、背景的context_id=6，礼物context_id=1
+
   if(asset[1]!=6){ return ;}
   for(var i=0;i<cards.length;i++){
     var _card=cards[i];
     if(asset[2] == _card.assetid){
       if(_card.market_hash_name == "753-Gems"){return;}
-      document.querySelector(".item_market_actions").firstChild.firstChild.firstChild.target="_blank";
+      var item_actions=document.querySelectorAll(".item_market_actions");
+      Array.prototype.forEach.call(
+        item_actions,
+        function(e){e.firstChild.firstChild.firstChild.target="_blank";});
+
+        
+        
       //steamcardexchange页面
+
       document.querySelector("#sce_page").href="http://www.steamcardexchange.net/index.php?gamepage-appid-"+_card.appid;
       document.querySelector("#sce_page .item_market_action_button_contents").textContent=_card.market_hash_name;
     }

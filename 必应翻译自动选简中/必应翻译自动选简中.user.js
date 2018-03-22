2588f753497eaec67d6d720843a0679428d0435f
abc45628 <abc45628@163.com>
2018-03-22 15:16:42 +0800

    更新 必应翻译自动选简中.user.js

--- 必应翻译自动选简中/必应翻译自动选简中.user.js
+++ 必应翻译自动选简中/必应翻译自动选简中.user.js
@@ -8,14 +8,30 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    var options=document.querySelector('select#t_tl').options;
-    for(var i=0;i<options.length;i++){
-        let o=options[i];
-        if(o.text==='简体中文'){
-            o.selected=true
-            break;
-        }
+(function () {
+  'use strict';
+  var options = document.querySelector('select#t_tl').options;
+  for (var i = 0; i < options.length; i++) {
+    let o = options[i];
+    if (o.text === '简体中文') {
+      o.selected = true;
+      break;
     }
+  }
+  var parent = document.querySelector('select#t_tl').parentElement;
+
+  var div = document.createElement('div');
+  div.innerHTML = '<input class="ttl_histbtn" value="英语" type="button" placeholder="en" data-bm="11">';
+  div.style.display = 'inline';
+  parent.appendChild(div);
+  div.addEventListener('click', function (event) {
+    var options = document.querySelector('select#t_tl').options;
+    for (var i = 0; i < options.length; i++) {
+      let o = options[i];
+      if (o.text === '英语') {
+        o.selected = true;
+        break;
+      }
+    }
+  });
 })();

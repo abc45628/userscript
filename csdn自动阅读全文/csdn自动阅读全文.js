c221f88aaaa624703cc69576999b155aa2b47ee8
abc45628 <abc45628@163.com>
2018-03-23 15:36:53 +0800

    更新 csdn自动阅读全文.js

--- csdn自动阅读全文/csdn自动阅读全文.js
+++ csdn自动阅读全文/csdn自动阅读全文.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://blog.csdn.net/*/article/details/*
+// @match        http*/://blog.csdn.net/*/article/details/*
 // @grant        none
 // ==/UserScript==
 

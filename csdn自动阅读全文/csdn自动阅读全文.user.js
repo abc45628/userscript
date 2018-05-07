a1588474bce50b09d5daa4a54a94f0143500fa25
abc45628 <abc45628@163.com>
2018-05-07 09:29:48 +0800

    1

--- csdn自动阅读全文/csdn自动阅读全文.user.js
+++ csdn自动阅读全文/csdn自动阅读全文.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         csdn自动阅读全文
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @updateURL    https://gitee.com/abc45628/userscript/raw/master/csdn自动阅读全文/csdn自动阅读全文.user.js
 // @match        http*://blog.csdn.net/*/article/details/*

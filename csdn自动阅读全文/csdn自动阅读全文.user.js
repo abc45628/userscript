66739ffa09f781620947fba084894d2649bd0e87
abc45628 <abc45628@163.com>
2018-05-07 09:32:48 +0800

    1

--- csdn自动阅读全文/csdn自动阅读全文.user.js
+++ csdn自动阅读全文/csdn自动阅读全文.user.js
@@ -10,8 +10,5 @@
 
 (function () {
   'use strict';
-  var btn = document.getElementsByClassName("btn btn-large btn-gray-fred read_more_btn");
-  if (btn && btn.length == 1) {
-    btn[0].click();
-  }
+  $('#btn-readmore').click();
 })();

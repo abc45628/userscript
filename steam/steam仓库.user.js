e97664a88ed0ce8967a99ee1f3b0ab48e1e9b523
abc45628 <abc45628@163.com>
2018-07-16 23:32:18 +0800

    重构1

--- steam/steam仓库.user.js
+++ steam/steam仓库.user.js
@@ -12,172 +12,149 @@
 // ==/UserScript==
 (function () {
 	"use strict";
-	const $J = unsafeWindow.$J;//jquery
-	const user64 = document.URL.match("[0-9]+")[0];//steam 64位id
-	// console.log(user64);
-	let getInvData_progress = false;//向服务器查询库存中
-	let loadCount = 0;//重试加载仓库数据的次数
-	let assets = [//库存数据
+	let $J = unsafeWindow.$J;//jquery
+	// console.log($J);
+	/**steam 64位id */
+	let user64 = document.URL.match("[0-9]+")[0];
+	// console.log('user64=' + user64);
+	/**仓库查询地址 */
+	let inv_url = "//steamcommunity.com/inventory/" + user64 + "/753/6";
+	/** 向服务器查询库存中*/
+	let getInvData_progress = false;
+	/** 如果请求的数量多于可读取到的卡片,不会返回more_items和last_assetid*/
+	let no_more_item = false;
+	/**重试加载仓库数据的次数 */
+	let loadCount = 0;
+	/**库存数据 */
+	let assets = [
 		//assetid这张卡的具体编号,就算是2张相同的卡,assetid都会不同
 		//classid该库存属于哪一种资产,相同的卡有相同的classid
 	];
-	let descriptions = [
+	let asset_list = [
 		//classid该库存属于哪一种资产,相同的卡有相同的classid
 		//tradable不可交易0,可交易1
 		//market_hash_name查价格用
 		//market_fee_app对应游戏appid
 	];
-	let has_more_item = true;//如果请求的数量多于可读取到的卡片,不会返回more_items和last_assetid
+	function asset() {
+		this.market_hash_name = undefined;
+		this.assetid = undefined;
+		this.appid = undefined;
+		return this;
+	}
+	/**查询时的资产起始id */
 	let start_assetid;
+	/**当前点击的资产 */
+	let market_hash_name;
+	init();
 
-	/**得到仓库数据
-	*  start_assetid   每张卡都有一个assetid，就算是2张相同的卡，assetid都会不同*/
-	function getInventoryData(start_assetid) {
-		if (getInvData_progress) { return; }
-		if (!has_more_item) { console.log("没有更多卡片"); return; }
-		let inv_url = "//steamcommunity.com/inventory/" + user64 + "/753/6";
-		let params = {
-			l: "schinese",//钱包区域
-			count: 75//请求的库存数量
-		};
-		if (start_assetid) {
-			params.start_assetid = start_assetid;
-		}
-
-		//开始查数据
-		getInvData_progress = true;
-		setTimeout(function () {
-			$J.get(inv_url, params).done(function (data) {
-				console.log("仓库数据");
-				console.log(data);
-				for (let i = 0; i < data.assets.length; i++) {
-					let ass = data.assets[i];
-					let asset = {};
-					asset.assetid = ass.assetid;
-					asset.classid = ass.classid;
-					assets.push(asset);
-				}
-				console.log("assets");
-				console.log(assets);
-				for (let j = 0; j < data.descriptions.length; j++) {
-					let de = data.descriptions[j];
-					let desc = {};
-					desc.classid = de.classid;
-					desc.tradable = de.tradable;
-					desc.market_hash_name = de.market_hash_name;
-					desc.market_fee_app = de.market_fee_app;
-					descriptions.push(desc);
-				}
-				console.log("descriptions");
-				console.log(descriptions);
-				has_more_item = false;
-				if (data.more_items) {
-					has_more_item = true;
-					start_assetid = data.last_assetid;
-				}
-				getInvData_progress = false;
-			}).error(function () {
-				console.error("仓库数据加载出错,开始id=" + start_assetid);
-				loadCount++;
-				getInvData_progress = false;
-				getInventoryData(start_assetid);
-			});
-		}, 1000 * (loadCount + 1));
+	function init() {
+		getInvData();
+		build_UI();
 	}
 
-	function build_UI() {
-		//steamcardexchange
-		let card_exchange_btn = document.createElement("div");
-		card_exchange_btn.innerHTML =
-			"<a id='sce_page' target='_blank' class='item_market_action_button item_market_action_button_green'>" +
-			"<span class='item_market_action_button_edge item_market_action_button_left'></span>" +
-			"<span class='item_market_action_button_contents'>steamcardexchange页面</span>" +
-			"<span class='item_market_action_button_edge item_market_action_button_right'></span>" +
-			"<span class='item_market_action_button_preload'></span></a>";
-		document.querySelector('.inventory_page_right').appendChild(card_exchange_btn);
-		//价位表
-		let price_list = document.createElement("div");
-		price_list.id = 'price_list';
-		price_list.style.marginTop = "46px";
-		price_list.innerHTML =
-			'<div style="float:left"><div id="market_commodity_forsale"></div>' +
-			'<div id="market_commodity_forsale_table"></div></div>' +
-			'<div style="float:left;margin-left: 30px;"><div id="market_commodity_buyrequests"></div>' +
-			'<div id="market_commodity_buyreqeusts_table"></div></div>';
-		document.querySelector('#inventory_pagecontrols').after(price_list);
-		//有价值的物品
-		let rich_item = document.createElement('div');
-		rich_item.id = 'rich_item';
-		document.getElementById("price_list").after(rich_item);
+	/**查仓库数据 */
+	function getInvData() {
+		if (getInvData_progress) { console.log('查询过程中,新请求被取消'); return; }
+		if (no_more_item) { console.log('没有更多卡片'); return; }
+		let data = {
+			l: "schinese",//钱包区域
+			count: 75,//请求的库存数量
+			start_assetid: start_assetid,
+		};
+		//开始查数据
+		getInvData_progress = true;
+		$J.ajax({
+			type: 'get',
+			url: inv_url,
+			data: data,
+			dataType: 'json',
+			success: function (resp) {
+				console.log("仓库数据:");
+				console.log(resp);
+				let assets = resp.assets;
+				let descriptions = resp.descriptions;
+				let last_assetid = resp.last_assetid;
 
-		document.addEventListener('click', function (ev) {
-			let evt = ev.target;
-			// 点击卡片
-			if (evt.className == 'inventory_item_link') {
-				click_card(evt.hash);
-			}
-			//点击下一页
-			else if (evt.id === 'pagebtn_next') {
-				click_next();
+				if (!last_assetid) { no_more_item = true; }
+				start_assetid = last_assetid;
+
+
+				for (let i = 0; i < descriptions.length; i++) {
+					const d = descriptions[i];
+					const a = assets[i];
+					let at = new asset();
+					at.market_hash_name = d.market_hash_name;
+					at.assetid = a.assetid;
+					at.appid = d.market_fee_app;
+					asset_list.push(at);
+				}
+				console.log('现有的资产：');
+				console.log(asset_list);
+				//step2 描述
+
+
+				//step3 资产
+
+
+				getInvData_progress = false;
+			},
+			error: function () {
+				console.error("仓库数据加载出错,请求参数为:");
+				console.error(data);
+				console.error("等待下一次请求。");
+				loadCount++;
+				getInvData_progress = false;
+				setTimeout(function () { getInvData(); }, 1000 * loadCount);
 			}
 		});
 	}
 
-	/**点击下一页 */
-	function click_next() {
-		let cur_page = document.querySelector('#pagecontrol_cur').textContent;
-		console.log("cur_page " + cur_page);
-		//剩余2页的时候再查后面的卡片
-		if (assets.length - cur_page * 25 <= 50) {
-			getInventoryData(start_assetid);
-		}
-	}
+
+
+
 
 	/**点击卡片 */
 	function click_card(url) {
-		console.log('url ' + url);
-		document.querySelector("#sce_page .item_market_action_button_contents").textContent = "steamcardexchange页面";
+		// console.log('url ' + url);
+		$J("#sce_page .item_market_action_button_contents").text("steamcardexchange页面");
 		//礼物context_id=1,优惠券context_id=3,卡片、宝珠、表情、背景的context_id=6
 		//753库存页面的调用id
-		let wanted_asset = url.match(/^#753_6_(.*)$/)[1];
-		if (!wanted_asset) { return; }
-		// console.log('wanted_asset ' + wanted_asset);
+		let clicked_asset_id = url.match(/^#753_6_(.*)$/)[1];
+		if (!clicked_asset_id) { console.error('没有assetid'); return; }
+		console.log('被点击的资产=' + clicked_asset_id);
 
 		//在社区市场查看  新窗口打开
-		let item_actions = document.querySelectorAll('.item_market_actions');
-		Array.prototype.forEach.call(item_actions, function (e) {
-			try {
-				e.firstChild.firstChild.firstChild.target = '_blank';
-			} catch (error) {
-				('第一次点击必然报错,什么都不需要做');
-			}
-		});
+		// let item_actions = document.querySelectorAll('.item_market_actions');
+		// Array.prototype.forEach.call(item_actions, function (e) {
+		// 	try {
+		// 		e.firstChild.firstChild.firstChild.target = '_blank';
+		// 	} catch (error) {
+		// 		('第一次点击必然报错,什么都不需要做');
+		// 	}
+		// });
 
 		//准备数据
-		let asset;
-		let desc;
-		for (let i = 0; i < assets.length; i++) {
-			if (wanted_asset != assets[i].assetid) { continue; }
-			asset = assets[i];
-
-			for (let j = 0; j < descriptions.length; j++) {
-				if (asset.classid != descriptions[j].classid) { continue; }
-				desc = descriptions[j];
-				break;
+		let choosen;
+		for (let i = 0; i < asset_list.length; i++) {
+			const asset = asset_list[i];
+			if (asset.assetid !== clicked_asset_id) {
+				continue;
 			}
-			break;
+			choosen = asset;
 		}
-		console.log('asset'); console.log(asset);
-		console.log('desc'); console.log(desc);
+
+		console.log('选中了');
+		console.log(choosen);
 
 		//steamcardexchange页面
-		if (asset && desc) {
-			document.querySelector("#sce_page").href = "http://www.steamcardexchange.net/index.php?gamepage-appid-" + desc.market_fee_app;
-			document.querySelector("#sce_page .item_market_action_button_contents").textContent = desc.market_hash_name;
-		}
+		// if (asset && desc) {
+		// 	document.querySelector("#sce_page").href = "http://www.steamcardexchange.net/index.php?gamepage-appid-" + choosen.appid;
+		// 	document.querySelector("#sce_page .item_market_action_button_contents").textContent = choosen.market_hash_name;
+		// }
 
-		//查市场价格
-		get_market_price(desc.market_hash_name);
+		// //查市场价格
+		// get_market_price(desc.market_hash_name);
 	}
 
 	/**查市场价格 */
@@ -209,7 +186,7 @@
 					console.error("价格数据加载出错,物品id=" + start_assetid);
 					loadCount++;
 					getInvData_progress = false;
-					getInventoryData(start_assetid);
+					getInventoryData111111(start_assetid);
 				}
 			});
 		});
@@ -269,7 +246,80 @@
 
 
 
-	getInventoryData(start_assetid);
-	build_UI();
-	console.log("finish");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/**点击下一页 */
+	function click_next() {
+		let cur_page = $J('#pagecontrol_cur').text();
+		console.log("cur_page=" + cur_page);
+		/**剩余n页的时候再查后面的卡片 */
+		let n = 2;
+		if (assets.length - cur_page * 25 <= 25 * n) {
+			getInvData();
+		}
+	}
+
+	function build_UI() {
+		//跳转steamcardexchange的按钮
+		let card_exchange_btn = document.createElement("div");
+		card_exchange_btn.innerHTML =
+			"<a id='sce_page' target='_blank' class='item_market_action_button item_market_action_button_green'>" +
+			"<span class='item_market_action_button_edge item_market_action_button_left'></span>" +
+			"<span class='item_market_action_button_contents'>steamcardexchange页面</span>" +
+			"<span class='item_market_action_button_edge item_market_action_button_right'></span>" +
+			"<span class='item_market_action_button_preload'></span></a>";
+		document.querySelector('.inventory_page_right').appendChild(card_exchange_btn);
+		//价位表
+		let price_list = document.createElement("div");
+		price_list.id = 'price_list';
+		price_list.style.marginTop = "46px";
+		price_list.innerHTML =
+			'<div style="float:left"><div id="market_commodity_forsale"></div>' +
+			'<div id="market_commodity_forsale_table"></div></div>' +
+			'<div style="float:left;margin-left: 30px;"><div id="market_commodity_buyrequests"></div>' +
+			'<div id="market_commodity_buyreqeusts_table"></div></div>';
+		document.querySelector('#inventory_pagecontrols').after(price_list);
+		//有价值的物品
+		let rich_item = document.createElement('div');
+		rich_item.id = 'rich_item';
+		document.getElementById("price_list").after(rich_item);
+
+		document.addEventListener('click', function (ev) {
+			let evt = ev.target;
+			// 点击卡片
+			if (evt.className == 'inventory_item_link') {
+				click_card(evt.hash);
+			}
+			//点击下一页
+			else if (evt.id === 'pagebtn_next') {
+				click_next();
+			}
+		});
+	}
 })();
+/*eslint-env greasemonkey */

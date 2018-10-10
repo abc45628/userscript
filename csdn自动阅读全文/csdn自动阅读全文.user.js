5adbb107ba047b003705b247bb146a004e5bc35a
abc45628 <abc45628@163.com>
2018-10-11 00:18:40 +0800

    add-屏蔽下方登录注册浮层

--- csdn自动阅读全文/csdn自动阅读全文.user.js
+++ csdn自动阅读全文/csdn自动阅读全文.user.js
@@ -1,8 +1,7 @@
 // ==UserScript==
 // @name         csdn自动阅读全文
-// @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  try to take over the world!
+// @namespace    https://gitee.com/abc45628/userscript
+// @version      2018.10.11.1
 // @updateURL    https://gitee.com/abc45628/userscript/raw/master/csdn自动阅读全文/csdn自动阅读全文.user.js
 // @match        http*://blog.csdn.net/*/article/details/*
 // @grant        none
@@ -10,5 +9,8 @@
 
 (function () {
   'use strict';
+  //正文部分阅读更多
   $('#btn-readmore').click();
+  //下方登录注册浮层
+  $('.btn-close').click();
 })();

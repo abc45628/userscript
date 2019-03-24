73a9a7a388d34509d53e95905e48e77d6044fa13
abc45628 <abc45628@163.com>
2019-03-24 20:54:04 +0800

    csdn自动阅读全文

--- csdn自动阅读全文/csdn自动阅读全文.user.js
+++ csdn自动阅读全文/csdn自动阅读全文.user.js
@@ -1,16 +1,23 @@
 // ==UserScript==
 // @name         csdn自动阅读全文
-// @namespace    https://gitee.com/abc45628/userscript
-// @version      2018.10.11.1
-// @updateURL    https://gitee.com/abc45628/userscript/raw/master/csdn自动阅读全文/csdn自动阅读全文.user.js
+// @namespace    https://github.com/abc45628/userscript
+// @version      2019.3.24.1
+// @updateURL    https://github.com/abc45628/userscript/raw/master/csdn自动阅读全文/csdn自动阅读全文.user.js
 // @match        http*://blog.csdn.net/*/article/details/*
+// @match        https://bbs.csdn.net/topics/*
+// @match        https://*.iteye.com/blog/*
 // @grant        none
 // ==/UserScript==
 
 (function () {
   'use strict';
   //正文部分阅读更多
-  $('#btn-readmore').click();
+  $(document.querySelector('#btn-readmore')).click()
   //下方登录注册浮层
   $('.btn-close').click();
+    $('.btn-close-ad').click();
+    //左侧猿学习
+    $('.fourth_column').remove();
+    //CSDN论坛阅读全文
+    $(".js_show_topic").click();
 })();
